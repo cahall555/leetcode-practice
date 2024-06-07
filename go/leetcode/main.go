f07b4081471cc4c261271ffc92ec0bafe347946c
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2024-06-07 17:06:58 -0400

    added linked lists.

--- go/leetcode/main.go
+++ go/leetcode/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"example.com/dynamic"
 	"example.com/recursive"
+	"example.com/linkedlist"
 )
 
 func main() {
@@ -20,4 +21,15 @@ func main() {
 	fmt.Println(recursive.Palindrome("racecar"))
 
 	fmt.Println(recursive.Fibonacci(6))
+
+	a := linkedlist.Node{Value: "a"}
+	b := linkedlist.Node{Value: "b"}
+	c := linkedlist.Node{Value: "c"}
+	d := linkedlist.Node{Value: "d"}
+
+	a.Next = &b
+	b.Next = &c
+	c.Next = &d
+
+	fmt.Println(linkedlist.LinkedListValues(&a))
 }
---
+++ go/linkedlist/llvalues.go
@@ -0,0 +1,16 @@
+package linkedlist //Article with linked lists in Go: https://edwinsiby.medium.com/understanding-linked-lists-in-go-a-comprehensive-guide-for-all-skill-levels-1e3d91a24d08
+
+
+func LinkedListValues(head *Node) []string {
+	var values []string
+	fillValues(head, &values)
+	return values
+} 
+
+func fillValues(head *Node, values *[]string) {
+	if head == nil {
+		return
+	}
+	*values = append(*values, head.Value)
+	fillValues(head.Next, values)
+}
